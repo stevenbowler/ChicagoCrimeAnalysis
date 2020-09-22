93844d20867b1ea5c4071571f71bd9ddc5ebd5ca
Steven Bowler <stevenbowler@yahoo.com>
2020-09-22 08:26:04 -0500

    first load of empty project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recent Trends in Chicago Crime',
+    author='Steven Bowler',
+    license='MIT',
+)
